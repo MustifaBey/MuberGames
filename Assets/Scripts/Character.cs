4a429fc2626bfd32373164f3f25b08bf658d0d2e
BerkBeyy <caliskanberk085@gmail.com>
2026-02-08 22:37:41 +0300

    karakter yürüme

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -2,15 +2,79 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Movement Settings")]
+    [SerializeField] private float speed = 5f;
+
+    private Vector3 targetPosition;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+
     void Start()
     {
-        
+        // Başlangıçta hedef konum mevcut konum olsun
+        targetPosition = transform.position;
+
+        // Bileşenleri al
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        HandleInput();
+        Move();
+    }
+
+    void HandleInput()
+    {
+        // Mouse sağ tık (1) basılı tutulursa veya tıklanırsa
+        if (Input.GetMouseButton(1))
+        {
+            // Mouse pozisyonunu dünya koordinatlarına çevir
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            // Z eksenini sıfırla (2D oyun için)
+            mousePos.z = 0;
+
+            targetPosition = mousePos;
+        }
+    }
+
+    void Move()
+    {
+        // Hedefe doğru hareket et
+        if (Vector3.Distance(transform.position, targetPosition) > 0.1f)
+        {
+            // Hareketi uygula
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+
+            // Animasyon kontrolü
+            if (animator != null)
+            {
+                animator.SetBool("IsRunning", true);
+            }
+
+            // Karakterin yönünü çevir (opsiyonel)
+            if (targetPosition.x < transform.position.x)
+            {
+                // Sola bakıyor
+                if (spriteRenderer != null) spriteRenderer.flipX = true;
+                else transform.localScale = new Vector3(-1, 1, 1);
+            }
+            else
+            {
+                // Sağa bakıyor
+                if (spriteRenderer != null) spriteRenderer.flipX = false;
+                else transform.localScale = new Vector3(1, 1, 1);
+            }
+        }
+        else
+        {
+            // Durma durumu
+            if (animator != null)
+            {
+                animator.SetBool("IsRunning", false);
+            }
+        }
     }
 }
